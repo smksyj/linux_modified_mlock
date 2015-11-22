f32bc2c65c46a040c2bcde921c29c611bf879f44
smksyj <smksyj@gmail.com>
2015-11-22 23:04:04 +0900

    first commit

---
+++ build_again.sh
@@ -0,0 +1,1 @@
+make -j8 && make -j8 modules_install && make install
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 20 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#20 SMP Sun Nov 22 21:07:47 KST 2015"
+#define LINUX_COMPILE_BY "hyeon0145"
+#define LINUX_COMPILE_HOST "keci"
+#define LINUX_COMPILER "gcc version 4.8.4 (Ubuntu 4.8.4-2ubuntu1~14.04) "
